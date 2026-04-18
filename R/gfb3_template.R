4e1c309ed60d5191b3c3faa011a9f58e3ff3d64a
Francisco Javier Rivas Fuenzalida <frivasfu@purdue.edu>
2026-04-18 13:56:03 -0400

    fix bug in gfb3_template()

--- R/gfb3_template.R
+++ R/gfb3_template.R
@@ -3,9 +3,6 @@
 #' @param n Integer. Number of empty rows to include (default 0).
 #' @return A tibble with all GFB3 columns and correct data types.
 #' @export
-#'
-#' PlotID	Latitude	Longitude	PA	TreeID	Species	Status	DBH	YR	PrevDBH	PrevYR
-
 gfb3_template <- function(n = 0L) {
   template <- tibble::tibble(
     # Plot metadata
